589e6d75f1227ef443927770da6e7807a3655e1a
Atharv <atharvpathak248@gmail.com>
2024-10-28 11:55:25 +0530

    CROS Problem Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'prompt',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
